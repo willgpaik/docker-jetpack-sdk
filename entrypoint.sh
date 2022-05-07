f564742d28156d894d09aac618910693230e2e57
willgpaik <44753879+willgpaik@users.noreply.github.com>
2022-05-07 04:00:11 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,13 @@ set -e
 
 sdkmanager \
 --cli install \
---user email@domain.com \
---password xyz \
 --logintype devzone \
 --product Jetson \
---version 4.3 \
+--version 4.6.2 \
 --targetos Linux \
 --host \
---target P3310 \
---flash \
+--target JETSON_TX2_TARGETS \
+--flash all \
 --license accept
 
 exec "$@"
